3b1194f1c34543357e6b7176a9e70cd712915ead
Jevgenijs Levconoks <J.Levconoks@gmail.com>
2018-12-28 15:34:12 +0000

    Initial proof of concept

---
+++ buildAndDeploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Building Alexa Bin Schedule Skill"
+
+env GOOS=linux GOARCH=amd64 go build .
+
+echo "Preparing zip package"
+zip -j alexa-bin-schedule-skill.zip alexa-bin-schedule-skill
+
+echo "Depoloying..."
+aws lambda update-function-code --function-name AlexaBinScheduleSkill --zip-file fileb://alexa-bin-schedule-skill.zip
+
+echo "Cleanup"
+rm alexa-bin-schedule-skill
+rm alexa-bin-schedule-skill.zip
+
+echo "Done!"
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"log"
+	"os"
+	"sort"
+	"time"
+)
+
+type Response struct {
+	Version string       `json:"version"`
+	Body    ResponseBody `json:"response"`
+}
+
+type ResponseBody struct {
+	OutputSpeech     Payload `json:"outputSpeech,omitempty"`
+	ShouldEndSession bool    `json:"shouldEndSession"`
+}
+
+type Payload struct {
+	Type string `json:"type,omitempty"`
+	Text string `json:"text,omitempty"`
+	Ssml string `json:"ssml"`
+}
+
+// Bin Schedule
+
+type BinScheduleDao struct {
+	DocumentId string   `json:"documentId"`
+	PremisesId string   `json:"premisesId"`
+	Brown      []string `json:"brown"`
+	Green      []string `json:"green"`
+	Black      []string `json:"black"`
+}
+
+type binSchedule struct {
+	DocumentId string
+	PremisesId string
+	schedule   []scheduleItem
+}
+
+type scheduleItem struct {
+	colour string
+	date   time.Time
+}
+
+func main() {
+	//response, _ := MyBinCollectionHandler()
+	//log.Println(response)
+	lambda.Start(MyBinCollectionHandler)
+}
+
+func MyBinCollectionHandler() (Response, error) {
+	premisesId, exist := os.LookupEnv("premisesid")
+	if !exist {
+		log.Fatal("Missing 'premisesid' parameter")
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := dynamodb.New(sess)
+	result, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("BinSchedule"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"documentId": {S: aws.String(fmt.Sprintf("LEEDS_%v", premisesId))},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := BinScheduleDao{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &r)
+	if err != nil {
+		log.Println("Error while unmarshalling", err)
+	}
+
+	schedule := r.toBinSchedule()
+	date := schedule.schedule[0].date
+	response := fmt.Sprintf("<speak>Your next bin is '%v', on %v <say-as interpret-as=\"date\">????%v</say-as></speak>", schedule.schedule[0].colour, date.Format("Monday"), date.Format("0102"))
+	return buildResponse(response), nil
+
+}
+
+func (dao BinScheduleDao) toBinSchedule() binSchedule {
+	result := binSchedule{DocumentId: dao.DocumentId, PremisesId: dao.PremisesId}
+
+	layout := "2006-01-02"
+
+	for _, v := range dao.Black {
+		t, err := time.Parse(layout, v)
+		if err != nil {
+			log.Println("Error parsing Date ", v)
+		}
+
+		result.schedule = append(result.schedule, scheduleItem{"Black", t})
+	}
+
+	for _, v := range dao.Green {
+		t, err := time.Parse(layout, v)
+		if err != nil {
+			log.Println("Error parsing Date ", v)
+		}
+
+		result.schedule = append(result.schedule, scheduleItem{"Green", t})
+	}
+
+	for _, v := range dao.Brown {
+		t, err := time.Parse(layout, v)
+		if err != nil {
+			log.Println("Error parsing Date ", v)
+		}
+
+		result.schedule = append(result.schedule, scheduleItem{"Brown", t})
+	}
+
+	sort.Slice(result.schedule, func(i, j int) bool {
+		return result.schedule[i].date.Before(result.schedule[j].date)
+	})
+
+	return result
+}
+
+func buildResponse(rsp string) Response {
+	return Response{
+		Version: "1.0",
+		Body: ResponseBody{
+			OutputSpeech: Payload{
+				Type: "SSML",
+				Ssml: rsp,
+			},
+			ShouldEndSession: true,
+		},
+	}
+}
